df87670490e83b384cdb329d97236336e0e12693
aidancer77 <107675611+aidancer77@users.noreply.github.com>
2023-02-26 17:51:37 +0300

    Delete MealPlan directory

--- MealPlan/src/main/java/com/example/demo/repository/DishCategoriesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.DishesCategories;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DishCategoriesRepository extends CrudRepository<DishesCategories, Long> {
-}
--- MealPlan/src/main/java/com/example/demo/repository/DishesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Dishes;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DishesRepository extends CrudRepository<Dishes,Long> {
-}
